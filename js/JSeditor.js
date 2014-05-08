fc89a4b7fd333aca3e182565d915f6d28c22860b
AndrewDuryea <andrew.duryea397@gmail.com>
2014-05-07 22:09:57 -0500

    separated the engine from the controller, now the controller should be useless

--- js/JSeditor.js
+++ js/JSeditor.js
@@ -7,9 +7,166 @@
 ************************************************************************************/
 
 function JSEditor(divID, chapterName, exerciseNum) {
+
+/* begin copy from old controller.js*****************/
+	var intervalID;
+	var codeStr;
+	var runMode = false;
+	var varArr = [];
+	var scopeArr = [];
+	var thisObj = this;
+	var lastLine = -1;
+	var firstMove = true;
+	var nextRowInd = 0;
+	var slidDown = false;
+	var editCellID;
+	var shiftDown = false;
+	var showVarBox = true;
+	var showScope = false;
+	var green = "#5CB85C";
+	var greenHover = "#47A447";
+	var orange = "#F0AD4E";
+	var orangeHover = "#F09C28";
+	var red = "#D9534F";
+	var redHover = "#D2322D";
+	//var figDiv = document.getElementById("fig" + sandboxNum + "Div");
+	var figDiv = document.getElementById(divID);
 	
-	var editor = new Editor(divID, chapterName, exerciseNum, true, true, 1, true, true, false);
+	figDiv.innerHTML = '<div id="selector" style="text-align:center"></div> \
+		<h4 id="codeTitle">Code Window</h4> \
+		<div class="codeAndButtons"> \
+			<div id="buttons"> \
+				<div><button id="fig' + divID + 'AddVar"        type="button">Variable</button></div> \
+				<div><button id="fig' + divID + 'AddArr"        type="button" onclick="figure.getEditor().addVariable("array")">Array</button></div> \
+				<div><button id="fig' + divID + 'AddFunc"       type="button" onclick="figure.getEditor().addFunction()">Declare Function</button></div> \
+				<div><button id="fig' + divID + 'Assign"        type="button" onclick="figure.getEditor().addOneLineElement("assignment")">Assign</button></div> \
+				<div><button id="fig' + divID + 'Write"         type="button" onclick="figure.getEditor().addOneLineElement("write")">Write</button></div> \
+				<div><button id="fig' + divID + 'Writeln"       type="button" onclick="figure.getEditor().addOneLineElement("writeln")">Writeln</button></div> \
+				<div><button id="fig' + divID + 'StringPrompt"  type="button" onclick="figure.getEditor().addOneLineElement("stringPrompt")">String Prompt</button></div> \
+				<div><button id="fig' + divID + 'NumericPrompt" type="button" onclick="figure.getEditor().addOneLineElement("numericPrompt")">Numeric Prompt</button></div> \
+				<div><button id="fig' + divID + 'While"         type="button" onclick="figure.getEditor().addWhile()">While</button></div> \
+				<div><button id="fig' + divID + 'AddFor"        type="button" onclick="figure.getEditor().addFor()">For</button></div> \
+				<div><button id="fig' + divID + 'AddIfThen"     type="button" onclick="figure.getEditor().addIfThen()">If...Then</button></div> \
+				<div><button id="fig' + divID + 'AddIfElse"     type="button" onclick="figure.getEditor().addIfElse()">If...Else</button></div> \
+				<div><button id="fig' + divID + 'FuncCall"      type="button" onclick="figure.getEditor().addOneLineElement("functionCall")">Call Function</button></div> \
+				<div><button id="fig' + divID + 'Return"        type="button" onclick="figure.getEditor().addOneLineElement("return")">Return</button></div> \
+			</div> \
+			<div id="fig' + divID + 'Editor" class="programCode"> </div>\
+		</div> \
+		<div id="fig' + divID + 'OutVarBox" class="outterDiv"> \
+		  <h4 id="varTitle">Variables</h4> \
+		  <div id="fig' + divID + 'VarBox" class="varDiv"> \
+			<table id="fig' + divID + 'VarTable" class="normal"></table> \
+		  </div> \
+		</div> \
+		<div class="outterDiv"> \
+			<h4 id="outTitle">Output</h4> \
+			<div id="fig' + divID + 'OutputBox" class="varDiv"> \
+				<table id="fig' + divID + 'OutputTable"></table> \
+			</div> \
+		</div> \
+		<div id="runWalk" align="center"> \
+		  <button id="fig' + divID + 'Run" type="button" style="color:#FFFFFF;background-color:' + green + '">Run</button> \
+		  <button id="fig' + divID + 'Walk" type="button" style="color:#FFFFFF;background-color:' + orange + '">Walk</button> \
+		</div>';
+   
+	//subract 6 for the width of the border of .textArea, if the border ever changes, this will have to change too
+	$('#fig' + divID + 'Editor').height($('#buttons').height() - 6);
+	
+	outputTable = document.getElementById("fig" + divID + "OutputTable");
+	var outputBox = document.getElementById("fig" + divID + "OutputBox");
+	//var outputBox = document.getElementById("fig" + sandboxNum + "OutputBox");
+	var row = outputTable.insertRow(0);
+	var cell = row.insertCell(0);
+	nextRowInd++;
+	varBox = document.getElementById("fig" + divID + "VarBox");
+	varTable = document.getElementById("fig" + divID + "VarTable");
+	
+	var addVarButton = document.getElementById("fig" + divID + "AddVar");
+	var addArrButton = document.getElementById("fig" + divID + "AddArr");
+	var addFuncButton = document.getElementById("fig" + divID + "AddFunc");
+	var assignButton = document.getElementById("fig" + divID + "Assign");
+	var writeButton = document.getElementById("fig" + divID + "Write");
+	var writelnButton = document.getElementById("fig" + divID + "Writeln");
+	var stringPromptButton = document.getElementById("fig" + divID + "StringPrompt");
+	var numericPromptButton = document.getElementById("fig" + divID + "NumericPrompt");
+	var whileButton = document.getElementById("fig" + divID + "While");
+	var forButton = document.getElementById("fig" + divID + "AddFor");
+	var ifThenButton = document.getElementById("fig" + divID + "AddIfThen");
+	var ifElseButton = document.getElementById("fig" + divID + "AddIfElse");
+	var funcCallButton = document.getElementById("fig" + divID + "FuncCall");
+	var returnButton = document.getElementById("fig" + divID + "Return");
+	//var walkButtonObj = document.getElementById("fig" + divID + "Walk");
+	//var runButtonObj = document.getElementById("fig" + divID + "Run");
+	
+	addVarButton.onclick = function () { addVariable("variable"); };
+	addArrButton.onclick = function () { addVariable("array"); };
+	addFuncButton.onclick = function () { addFunction(); };
+	assignButton.onclick = function () { addOneLineElement("assignment"); };
+	writeButton.onclick = function () { addOneLineElement("write"); };
+	writelnButton.onclick = function () { addOneLineElement("writeln"); };
+	stringPromptButton.onclick = function () { addOneLineElement("stringPrompt"); };
+	numericPromptButton.onclick = function () { addOneLineElement("numericPrompt"); };
+	whileButton.onclick = function() { addWhile(); };
+	forButton.onclick = function() { addFor(); };
+	ifThenButton.onclick = function() { addIfThen(); };
+	ifElseButton.onclick = function() { addIfElse(); };
+	funcCallButton.onclick = function () { addOneLineElement("functionCall"); };
+	returnButton.onclick = function () { addOneLineElement("return"); };
+	//walkButtonObj.onclick = function() { walkButton(); }
+	//runButtonObj.onclick = function() { runButton(); }
+	
+	$(document).ready(function() {
+            $("#fig" + divID + "OutVarBox").hide(); 
+	 });
+	 	
+	// Walk button listener
+	$("#fig" + divID + "Walk").click(function() {
+		engine.walkButton();
+	});
+	
+	// Run button listener
+	$("#fig" + divID + "Run").click(function() {
+		$("#fig" + divID + "OutVarBox").slideUp(function() {
+			engine.runButton();
+			slidDown = false;
+		});
+	});
+	
+	$("#fig" + divID + "OutVarBox").slideUp("medium");
+	
+	// Mouse events for buttons
+	$("#fig" + divID + "Run").mousemove(function() {
+		var button = document.getElementById("fig" + divID + "Run");
+		
+		if (button.textContent == "Run") button.style.backgroundColor = greenHover;
+		else button.style.backgroundColor = orangeHover;
+	});
+	
+	$("#fig" + divID + "Run").mouseout(function() {
+		var button = document.getElementById("fig" + divID + "Run");
+		if (button.textContent == "Run") button.style.backgroundColor = green;
+		else button.style.backgroundColor = orange;
+	});
+
+	$("#fig" + divID + "Walk").mousemove(function() {
+		var button = document.getElementById("fig" + divID + "Walk");
+		if (button.textContent == "Walk") button.style.backgroundColor = orangeHover;
+		else button.style.backgroundColor = redHover;
+	});
+	
+	$("#fig" + divID + "Walk").mouseout(function() {
+		var button = document.getElementById("fig" + divID + "Walk");
+		if (button.textContent == "Walk") button.style.backgroundColor = orange;
+		else button.style.backgroundColor = red;
+	});
+	
+/* end copy from old controller.js*****************/
+
+	var editor = new Editor("fig" + divID + "Editor", chapterName, exerciseNum, true, true, 1, true, true, false);
 	editor.loadEditor('figcontainer-exer' + exerciseNum + 'Editor', divID, true);
+	
+	var engine = new Engine(divID, editor);
 	
 	var variableCount = 0;									// keeps count of the amount of variables
 	var funcCount = 0;										// keeps count of number of functions
@@ -786,7 +943,7 @@ function JSEditor(divID, chapterName, exerciseNum) {
 	function canInsertBefore(row){
 		if (row.length <= 0) return false;
 		if (row[0].indexOf("//") >= 0) return false;
-		if (row[0] == 'var') return false;
+		if (row.indexOf('var') >= 0) return false;
 		if (row[0] == 'function') return false;
 		if (row[0] == '&nbsp;')  return false;
 		
@@ -800,7 +957,7 @@ function JSEditor(divID, chapterName, exerciseNum) {
 	
 	function canInsertAfter(row){
 		if (row.length <= 0) return false;
-		if (row[0] == 'var') return false;
+		if(row[0] == 'var') return false;
 		if (row[0] == 'function') return false;
 		if (row[0] == '&nbsp;')  return false;
 		
@@ -2217,7 +2374,6 @@ function JSEditor(divID, chapterName, exerciseNum) {
 				else {
 					if (row[j].indexOf(";") >= 0) {
 						semi = row[j].indexOf(";");
-						for (var k = 0; k < semi + 1; k++) tempText += row[j].charAt(k);
 						
 						for (var k = 0; k < semi + 1; k++)
 							tempText += row[j].charAt(k);
@@ -3608,36 +3764,45 @@ function JSEditor(divID, chapterName, exerciseNum) {
         }
     }
     
-  function createSelector(title, optionS, callback) {
-        var newSel = new Selector();
-        newSel.open(title, optionS, callback, document.getElementById(divID));
-  }
-    
-  function createStringPad(title, instructions, callback) {
-        var newStrP = new StringPad();
-        newStrP.open(title, instructions, callback, document.getElementById(divID));
-  }
-    
-  function createNumPad(minValue, maxValue, titleStr, instructions, decimalAllowed, base, callback) {
+	function createSelector(title, optionS, callback) {
+		var newSel = new Selector();
+		newSel.open(title, optionS, callback, document.getElementById(divID));
+	}
+
+	function createStringPad(title, instructions, callback) {
+		var newStrP = new StringPad();
+		newStrP.open(title, instructions, callback, document.getElementById(divID));
+	}
+
+	function createNumPad(minValue, maxValue, titleStr, instructions, decimalAllowed, base, callback) {
 		var newNumpad = new NumberPad();
 		newNumpad.open(minValue, maxValue, titleStr, instructions, decimalAllowed, base, callback, document.getElementById(divID));
-   }
+	}
 
-  function createAlertBox(title, msg, bool, callback) {
-        var alert = new Alert();
-        alert.open(title, msg, bool, callback, document.getElementById(divID));
-  }
-  
-  function dummy(result){
+	function createAlertBox(title, msg, bool, callback) {
+		var alert = new Alert();
+		alert.open(title, msg, bool, callback, document.getElementById(divID));
+	}
+
+	function dummy(result){
 	return;
-  }
+	}
 
+	this.retrieveUpdates = retrieveUpdates;
+	function retrieveUpdates(){
+		editor.loadEditor("figJSSandboxEditor", "fig" + divID + "Editor", true);
+	}
+	
+	this.saveEditor = saveEditor;
+	function saveEditor(){
+		if(editor.checkEditorData(true))
+			editor.saveEditor(true);
+	}
+	
 	this.clearEditor = clearEditor;
 	function clearEditor(){
-		console.log("here2");
 		editor.clearEditor();
-		editor = new Editor(divID, chapterName, exerciseNum, true, true, 1, true, true, true);
-		//init();
+		editor = new Editor("fig" + divID + "Editor", chapterName, exerciseNum, true, true, 1, true, true, true);
 	}
 }
 
